9968a18656d6a347eca1cb55641e5a53d7ae0c66
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-09-10 22:21:24 +0530

    java fundamentals

---
+++ HelloWorld/run.bat
@@ -0,0 +1,6 @@
+@echo off 
+
+javac *.java
+java HelloWorld
+
+Del *.class
---
+++ Learn_IT_Fundamentals/Learn_IT_Fundamentals/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * Hello World Application
+ * 
+ * @author Bhaskar Mudaliyar
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args -- input parameters
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * Entry point of application
+		 */
+
+		System.out.println("Hello World");
+
+		// TODO Add code here in future
+	}
+}
